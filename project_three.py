8e00c2cd41c1a0b0e80ce7a647968e41e47fd0b2
loveforggong@gmail.com <loveforggong@gmail.com>
2026-04-26 08:52:58 +0900

    feat: 손익 및 수익률 계산

--- project_three.py
+++ project_three.py
@@ -34,12 +34,20 @@ def get_single_price_api(ticker):
     return data['trade_price']
 
 def calculate_investment_return(money_invest, ticker, day_invest):
-    #과거투자시점 가격
+    #과거투자시점 가격 목록
     money_past = get_historical_data_api(ticker, day_invest)
+    #나의 투자시점 가격
     volumes = money_invest / money_past[-1]
     #현재 가치 계산
-    money_today = volumes * money_past[0]
-    print(f"현재가치: {money_today}")
+    money_today = volumes * money_past[0]    
+    
+    #손익 및 수익률 계산
+    #손익
+    profit_loss = money_today - money_invest
+    #수익률
+    rate_profit = (profit_loss / money_invest) * 100
+    print(f"수익률: {rate_profit}")
+    
     
     
 ticker = "KRW-BTC"
