e976ba3267398b82bf46a2386d946b6284e10b18
loveforggong@gmail.com <loveforggong@gmail.com>
2026-04-26 08:10:37 +0900

    feat: 2. 과거 데이터 조회 함수 작성

--- project_three.py
+++ project_three.py
@@ -2,3 +2,21 @@ import requests
 from datetime import datetime
 import time
 import json
+
+def get_historical_data_api(ticker, days_count):
+    url = "https://api.upbit.com/v1/candles/days"
+
+    params = {
+        "market": ticker,
+        "count": days_count
+    }
+
+    response = requests.get(url, params=params)
+    candle_data = response.json()
+
+    prices = []
+
+    for data in candle_data:
+        prices.append(data["trade_price"])
+
+    return prices
