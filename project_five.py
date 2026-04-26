04098f0222a3c42da927c29ca2b1f5da4e2eeeff
loveforggong@gmail.com <loveforggong@gmail.com>
2026-04-26 15:24:39 +0900

    error: 날짜데이터를 잘못가져와서 수정 후 문제생김. 날짜가 str타입이라 rolling().mean() 평균값을 못구함.

--- project_five.py
+++ project_five.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import time
 import json
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def get_candle_data_api(ticker, count):
     
@@ -23,11 +24,11 @@ ticker = "KRW-BTC"
 count = 30
 
 data = get_candle_data_api(ticker, count)
-
+print(f"데이터 {data}")
 prices_date = []
 
 for item in data:      
-    date = item['candle_acc_trade_volume']
+    date = item['candle_date_time_kst']
     prices = item['trade_price']
 
     #print(f"종가 : {prices}, 날짜 : {date} ")
@@ -35,11 +36,11 @@ for item in data:
     prices_date.append([date, prices])
 
 #print(f"날짜 종가 리스트 : {prices_date}")
-pd_prices_date = pd.DataFrame(prices_date)
+#pd_prices_date = pd.DataFrame(prices_date, columns=["날짜", "종가"])
 #print(pd_prices_date)
 
-five_data = pd_prices_date.rolling(5).mean()
-ten_data = pd_prices_date.rolling(10).mean()
+#five_data = pd_prices_date.rolling(5).mean()
+#ten_data = pd_prices_date.rolling(10).mean()
 
-print(f"5일선 : {five_data}")
-print(f"10일선 : {ten_data}")
+#print(f"5일선 : {five_data}")
+#print(f"10일선 : {ten_data}")
