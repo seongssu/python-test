ed8b16511afdf8c0d1ddbf1a7671d06df9bc444d
loveforggong@gmail.com <loveforggong@gmail.com>
2026-04-26 09:44:44 +0900

    feat: 함수 변경(현재가도 받아와야 하기 때문에)

--- project_four.py
+++ project_four.py
@@ -15,8 +15,14 @@ def get_historical_close_api(ticker, days_ago):
 
     response = requests.get(url, params=params)
     candle_data = response.json()
+    
+    current_price = candle_data[0]["trade_price"]
+    past_price = candle_data[-1]["trade_price"]
 
-    return candle_data[-1]["trade_price"]
+    return {
+        "current_price": current_price, 
+        "past_price": past_price
+        }
 
 #비교할 암호화폐 목록(리스트)
 tickers = ["KRW-BTC", "KRW-ETH", "KRW-XRP", "KRW-SOL"]
