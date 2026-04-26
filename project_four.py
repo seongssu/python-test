e7bd5399b6482af5f7b20805757de1fa7569c27d
loveforggong@gmail.com <loveforggong@gmail.com>
2026-04-26 09:34:50 +0900

    feat: N일전 주식별 종가 함수 생성: 확인완료

--- project_four.py
+++ project_four.py
@@ -2,3 +2,27 @@ import requests
 from datetime import datetime
 import time
 import json
+
+import requests
+
+def get_historical_close_api(ticker, days_ago):
+    url = "https://api.upbit.com/v1/candles/days"
+
+    params = {
+        "market": ticker,
+        "count": days_ago
+    }
+
+    response = requests.get(url, params=params)
+    candle_data = response.json()
+
+    return candle_data[-1]["trade_price"]
+
+#비교할 암호화폐 목록(리스트)
+tickers = ["KRW-BTC", "KRW-ETH", "KRW-XRP", "KRW-SOL"]
+days_ago = 30
+
+#호출 함수에서 ticker가 str타입으로 받아야해서 for문 돌림
+for item in tickers:
+    print(f"잘 작동하나? {get_historical_close_api(item, days_ago)}")
+    
