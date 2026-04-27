fa42e643e2f930c627a5db58d35081ae35a3b235
loveforggong@gmail.com <loveforggong@gmail.com>
2026-04-27 10:57:05 +0900

    feat: 보유 비중 상위 1개 코인(완료)

--- p01_week_project/p06_daily_report_portfolio/daily_report_portfolio.py
+++ p01_week_project/p06_daily_report_portfolio/daily_report_portfolio.py
@@ -53,7 +53,9 @@ def analyze_portfolio(portfolio):
     print(f"\n\n {"코인":<10} {"수량":>5} {"현재가":>21} {"가치":>20} {"비중":>14}")
     print("-" * 100)
 
-    
+    ratio_coin = {}
+    all_ratio = []
+    #비중 = 내가보유중인 코인중의 현재 가치 비율
     for x in portfolio_analysis:
         stocks = x["코인"]
         volumes = x["수량"]
@@ -62,10 +64,20 @@ def analyze_portfolio(portfolio):
         ratio = x["비중"]
 
         print(f"{stocks:<10} {volumes:>10}주 {current_price:>20}원 {value:>20}원 {ratio:>15.1f}%")
-
+        all_ratio.append(ratio)
+        ratio_coin[ratio] = stocks
+        
+    max_ratio_coin = max(all_ratio)
+    max_coin = ratio_coin[max_ratio_coin]
+    
+    #print(f"최대 비중인 코인 : {max_coin}")
+    return max_coin
+        
 portfolio = {
     "KRW-BTC":0.1,
     "KRW-ETH":3,
     "KRW-XRP":1000
 }
-analyze_portfolio(portfolio)
+
+max_coin = analyze_portfolio(portfolio)
+print(f"보유 비중 상위 1개 코인 : {max_coin}")  
