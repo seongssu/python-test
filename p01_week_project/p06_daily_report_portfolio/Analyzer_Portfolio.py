8016ab61c11351d4d7de32e70b113eccd0892641
loveforggong@gmail.com <loveforggong@gmail.com>
2026-04-27 13:08:04 +0900

    ref: 함수들 다른 파일로 분리하고 import하기

---
+++ p01_week_project/p06_daily_report_portfolio/Analyzer_Portfolio.py
@@ -0,0 +1,55 @@
+import requests
+from datetime import datetime
+import time
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+from UPbit_API import get_current_prices_api
+def analyze_portfolio(portfolio):
+
+    tickers = list(portfolio.keys())
+
+    prices = get_current_prices_api(tickers)
+
+    portfolio_analysis = []
+    total_value = 0
+
+    for stocks, volumes in portfolio.items():
+
+      current_price = prices.get(stocks,0)
+      value = current_price * volumes
+
+      portfolio_analysis.append({
+        "코인" : stocks,
+        "수량" : volumes,
+        "현재가격" : current_price,
+        "가치" : value
+  })
+
+      total_value += value
+
+    for x in portfolio_analysis:
+        x["비중"] = (x["가치"]/total_value) * 100
+
+    print(f"\n\n {"코인":<10} {"수량":>5} {"현재가":>21} {"가치":>20} {"비중":>14}")
+    print("-" * 100)
+
+    ratio_coin = {}
+    all_ratio = []
+    #비중 = 내가보유중인 코인중의 현재 가치 비율
+    for x in portfolio_analysis:
+        stocks = x["코인"]
+        volumes = x["수량"]
+        current_price = x["현재가격"]
+        value = x["가치"]
+        ratio = x["비중"]
+
+        print(f"{stocks:<10} {volumes:>10}주 {current_price:>20}원 {value:>20}원 {ratio:>15.1f}%")
+        all_ratio.append(ratio)
+        ratio_coin[ratio] = stocks
+        
+    max_ratio_coin = max(all_ratio)
+    max_coin = ratio_coin[max_ratio_coin]
+    
+    #print(f"최대 비중인 코인 : {max_coin}")
+    return max_coin
---
+++ p01_week_project/p06_daily_report_portfolio/UPbit_API.py
@@ -0,0 +1,62 @@
+import requests
+from datetime import datetime
+import time
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 2. 현재가 조회 함수
+def get_current_prices_api(tickers):
+
+    url = "https://api.upbit.com/v1/ticker"
+    headers = {"accept": "application/json"}
+
+    markets_param = ",".join(tickers)
+    params = {"markets": markets_param}
+
+    response = requests.get(url, headers=headers, params=params)
+    prices_data = response.json()
+
+    prices = {}
+    for data in prices_data:
+        prices[data['market']] = data['trade_price']
+
+    return prices
+
+def get_historical_close_api(portfolio, days_ago):
+    url = "https://api.upbit.com/v1/candles/days"
+
+    result = {}
+    for ticker in portfolio:
+        params = {
+            "market": ticker,
+            "count": days_ago
+        }
+
+        response = requests.get(url, params=params)
+        candle_data = response.json()
+    
+        current_price = candle_data[0]["trade_price"]
+        past_price = candle_data[-1]["trade_price"]
+        
+        result[ticker] = {
+            "current_price" : current_price,
+            "past_price" : past_price
+        }
+        
+
+    return result
+
+def get_candle_data_api(ticker, count):
+
+    url = "https://api.upbit.com/v1/candles/days"
+    headers = {"accept": "application/json"}
+    params = {
+        "market" : ticker,
+        "count" : count
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()
+
+    return data
