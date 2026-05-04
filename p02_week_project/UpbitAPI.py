c9937951524aacac26e85bf6d92aea69ec97d1a4
loveforggong@gmail.com <loveforggong@gmail.com>
2026-05-04 20:00:37 +0900

    feat: 일 캔들 조회 메서드(get_candle_data) 추가

--- p02_week_project/UpbitAPI.py
+++ p02_week_project/UpbitAPI.py
@@ -2,10 +2,11 @@ import requests
 import json
 
 class Upbit_API:
-    def __init__(self, tickers):
+    def __init__(self, tickers, count):
         self.tickers = tickers
+        self.count = count
     # 현재가 조회 : https://docs.upbit.com/kr/reference/list-tickers
-    def get_current_prices_api(self):
+    def get_current_prices(self):
         
         url = "https://api.upbit.com/v1/ticker"
         params = {'markets' : ",".join(self.tickers)}
@@ -36,5 +37,19 @@ class Upbit_API:
         return krw_tickers_list
     
     # 일 캔들 조회 : https://docs.upbit.com/kr/reference/list-candles-days
-    
+    def get_candle_data(self):
+        
+        url = "https://api.upbit.com/v1/candles/days"
+        params = {
+            "market" : self.tickers,
+            "count" : self.count
+        }
+        headers = {'accept': 'application/json'}
+        
+        response = requests.get(url, params=params, headers= headers)
+        
+        response.raise_for_status()
+        candle_data = response.json()
+        
+        return candle_data
         
