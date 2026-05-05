dd2afcd9804387dde064c94c6bdd6b28631f2a9a
loveforggong@gmail.com <loveforggong@gmail.com>
2026-05-05 23:03:15 +0900

    feat: Upbit_API클래스 내부의 메서드들 모두 예외처리 함수(error_handling())를 재사용할 수 있도록 했습니다.

--- p02_week_project/basic_func.py
+++ p02_week_project/basic_func.py
@@ -1,6 +1,26 @@
 import pandas as pd
+import requests
 
 def conversion_df (dict_data):
     df = pd.DataFrame(dict_data)
     
     return df
+
+def error_handling(url, params, headers):
+    try:
+        response = requests.get(url, params= params, headers= headers)
+        response.raise_for_status()
+        
+        data = response.json()    
+    
+        if not data:
+                raise ValueError("데이터가 존재하지 않습니다.")
+        
+        return data
+    
+    except Exception as e:
+        print(f"ERROR : {e}")
+        
+    return None            
+        
+        
