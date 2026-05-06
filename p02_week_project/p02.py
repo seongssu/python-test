9522c6f7296d8ab915cec418373e86d442c18a2a
loveforggong@gmail.com <loveforggong@gmail.com>
2026-05-06 17:55:57 +0900

    feat: 각 코인별 전체 평균이아니라 5일씩 데이터 이동 평균값으로 수정했습니다.

--- p02_week_project/p02.py
+++ p02_week_project/p02.py
@@ -6,15 +6,17 @@ from basic_func import error_handling
 import pandas as pd
 
 tickers = ["KRW-BTC","KRW-ETC","KRW-XRP"]
-count = 5
+count = 30
 
 upbit_api = Upbit_API(tickers)
 
 close_five_days = upbit_api.get_multi_candle_data(count)
 db_close_five_days = pd.DataFrame(close_five_days)
 #ticker별 close기준 데이터를 구해야하므로 그룹화 합니다.
-#5일치 평균을 계산합니다.
-close_for_ticker = db_close_five_days.groupby("market")["trade_price"].mean()
+#5일씩 이동 평균을 계산합니다.
+close_for_ticker = db_close_five_days.groupby("market")["trade_price"].rolling(5).mean()
+#결측치 확인
+check_close_for_ticker = close_for_ticker.isnull()
 
 print(f"5일간 이동평균선\n{close_for_ticker}")
-
+print(f"결측치 수 : {check_close_for_ticker}")
