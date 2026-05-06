a8942e4a6bf95f0e32b6d12bcb72aa1144ae1584
loveforggong@gmail.com <loveforggong@gmail.com>
2026-05-06 11:18:18 +0900

    memo: 문제 1번 완료

--- p02_week_project/main.py
+++ p02_week_project/main.py
@@ -9,6 +9,8 @@ tickers = ["KRW-BTC","KRW-ETC","KRW-XRP"]
 count = 30
 upbit_api = Upbit_API(tickers)
 
+###문제 1번###
+
 #30일간 tickers의 캔들 데이터 수집
 multi_candle_prices = upbit_api.get_multi_candle_data(count)
 df_multi_candle_prices = pd.DataFrame(multi_candle_prices)
@@ -37,3 +39,5 @@ print(
         ]
     ]
 )
+
+###문제 2번###
