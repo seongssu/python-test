7a165957d8ff47d669cf28b67d7b4e205ac2b5c7
loveforggong@gmail.com <loveforggong@gmail.com>
2026-05-06 18:47:06 +0900

    feat: 결측치 데이터 수정(fillna)

--- p02_week_project/p02.py
+++ p02_week_project/p02.py
@@ -15,14 +15,15 @@ db_close_five_days = pd.DataFrame(close_five_days)
 #ticker별 close기준 데이터를 구해야하므로 그룹화 합니다.
 #5일씩 이동 평균을 계산합니다.
 close_for_ticker = db_close_five_days.groupby("market")
-#결측치 확인
-#check_close_for_ticker = close_for_ticker.isnull()
 
-#print(f"5일간 이동평균선\n{close_for_ticker}")
-#print(f"결측치 수 : {check_close_for_ticker}")
-
+#결측치 처리(fillna) : 결측치 -> 해당날짜의 close값으로 대체하기
+db_close_five_days["ma5"] = None
 for market, group in db_close_five_days.groupby("market"):
     ma5 = group["trade_price"].rolling(5).mean()
     
-    print(f"\n코인명: {market}")
-    print(ma5)
+    ma5 = ma5.fillna(group["trade_price"])
+## ▶ 주의 : 아래처럼 하면 market이 다른걸로 넘어가면 처음부터 다시 덮어쓴다. 
+##    db_close_five_days["ma5"] = ma5
+    db_close_five_days.loc[group.index, "ma5"] = ma5
+    
+print(db_close_five_days["ma5"])
