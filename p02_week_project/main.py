752f34a26f406591a4d4b53b553f24e79699b9e3
loveforggong@gmail.com <loveforggong@gmail.com>
2026-05-06 19:25:53 +0900

    ref: p_two의 기능 분리 및 함수를 반환하도록 수정, main에서 제어 및 프린트

--- p02_week_project/main.py
+++ p02_week_project/main.py
@@ -3,10 +3,11 @@ from p02 import p_two
 ###문제 1번###
 # market : 코인명 / price_change : 종가 - 시가
 # price_change_pct : 변동률 / high_low_diff : 일일 변동폭
-p_one()
-
+result_p_one = p_one()
+print(result_p_one)
 
 ###문제 2번###
 # market : 코인명 / trade_price : 종가
 # ma5 : 종가 기준 5일 이동 평균
-p_two()
+result_p_two = p_two()
+print(result_p_two)
--- p02_week_project/p02.py
+++ p02_week_project/p02.py
@@ -13,10 +13,21 @@ def p_two():
 
     close_five_days = upbit_api.get_multi_candle_data(count)
     db_close_five_days = pd.DataFrame(close_five_days)
-    #ticker별 close기준 데이터를 구해야하므로 그룹화 합니다.
-    #5일씩 이동 평균을 계산합니다.
-    close_for_ticker = db_close_five_days.groupby("market")
 
+    calculated_isnull = calculate_isnull(db_close_five_days)
+        
+    #print(db_close_five_days["ma5"])
+    #print(f"컬럼명 : {db_close_five_days.columns}")
+    db_close_five_days = calculated_isnull[[
+        "market",
+        "trade_price",
+        "ma5"
+    ]]
+    
+    return db_close_five_days
+    
+def calculate_isnull(db_close_five_days):
+    
     #결측치 처리(fillna) : 결측치 -> 해당날짜의 close값으로 대체하기
     db_close_five_days["ma5"] = None
     for market, group in db_close_five_days.groupby("market"):
@@ -25,13 +36,7 @@ def p_two():
         ma5 = ma5.fillna(group["trade_price"])
     ## ▶ 주의 : 아래처럼 하면 market이 다른걸로 넘어가면 처음부터 다시 덮어쓴다. 
     ##    db_close_five_days["ma5"] = ma5
-        db_close_five_days.loc[group.index, "ma5"] = ma5
-        
-    #print(db_close_five_days["ma5"])
-    #print(f"컬럼명 : {db_close_five_days.columns}")
-    db_close_five_days = db_close_five_days[[
-        "market",
-        "trade_price",
-        "ma5"
-    ]]
-    print(db_close_five_days)
+        db_close_five_days.loc[group.index, "ma5"] = ma5  
+    
+    return db_close_five_days     
+    
