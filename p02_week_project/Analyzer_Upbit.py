0a0f7afe85e780b8093c243c8e259b1afe5b60c5
loveforggong@gmail.com <loveforggong@gmail.com>
2026-05-06 11:12:48 +0900

    feat: 일일 변동가격 계산 및 컬럼 추가

--- p02_week_project/Analyzer_Upbit.py
+++ p02_week_project/Analyzer_Upbit.py
@@ -6,9 +6,13 @@ class Analyzer_Upbit:
         self.df_multi_candle_prices = df_multi_candle_prices
     
     def get_price_change(self):
-        self.df_multi_candle_prices["price_change"] = self.df_multi_candle_prices["opening_price"] - self.df_multi_candle_prices["trade_price"]
-        print(f"{self.df_multi_candle_prices[["market", "price_change"]]}")
+        self.df_multi_candle_prices["price_change"] = self.df_multi_candle_prices["trade_price"] - self.df_multi_candle_prices["opening_price"]
+        return self.df_multi_candle_prices["price_change"]
         
     def get_price_change_pct(self):
         self.df_multi_candle_prices["price_change_pct"] = (self.df_multi_candle_prices["price_change"] / self.df_multi_candle_prices["opening_price"]) * 100
-        print(f"변동률 : {self.df_multi_candle_prices["price_change_pct"]}")
+        return self.df_multi_candle_prices["price_change_pct"]    
+    
+    def get_high_low_diff(self):
+        self.df_multi_candle_prices["high_low_diff"] = self.df_multi_candle_prices["high_price"] - self.df_multi_candle_prices["low_price"]
+        return self.df_multi_candle_prices["high_low_diff"]
