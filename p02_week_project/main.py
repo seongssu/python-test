55ca3e090a3cae2364e2a619a91d55b44584e388
loveforggong@gmail.com <loveforggong@gmail.com>
2026-05-09 03:10:02 +0900

    feat: 1. 티커별 데이터 정렬 후 차트 표현 2. x축 : 날짜, y축 : 가격으로 종가와 5일 이동 평균 "bar"형태로 차트 작성

--- p02_week_project/main.py
+++ p02_week_project/main.py
@@ -1,9 +1,10 @@
 from p01 import p_one
 from p02 import p_two
+from p03 import p_three
 import pandas as pd
 from basic_func import conversion_time
 from Sqlite_Upbit import Sqlite_Upbit
-###문제 1번###
+###▶▶▶▶▶문제 1번◀◀◀◀◀###
 # candle_date_time_kst : 거래 날짜
 # ticker : 코인명 / price_change : 종가 - 시가
 # price_change_pct : 변동률 / high_low_diff : 일일 변동폭
@@ -11,7 +12,7 @@ result_p_one = p_one()
 result_p_one["candle_date_time_kst"] = conversion_time(result_p_one["candle_date_time_kst"])
 print(result_p_one)
 
-###문제 2번###
+###▶▶▶▶▶문제 2번◀◀◀◀◀###
 # candle_date_time_kst : 거래 날짜
 # ticker : 코인명 / trade_price : 종가
 # ma5 : 종가 기준 5일 이동 평균
@@ -31,4 +32,8 @@ sqlite_upbit.save_to_database()
 
 # db에 저장된 dataframe확인하기
 saved_data = sqlite_upbit.load_from_database()
-print(f"저장된 데이터 \n{saved_data}")
+#print(f"저장된 데이터 \n{saved_data}")
+
+###▶▶▶▶▶문제 3번◀◀◀◀◀###
+result_p_three = p_three(result_all_data)
+#print(f"문제3번{result_p_three}")
---
+++ p02_week_project/p03.py
@@ -0,0 +1,25 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def p_three(result_all_data):
+    
+    result_all_data = result_all_data.sort_values(
+        ["ticker", "candle_date_time_kst"]
+    )
+    
+    result_grouby_ticker = result_all_data.groupby("ticker")
+    
+    for ticker, column in result_grouby_ticker:
+        
+        column.plot(
+            x = "candle_date_time_kst",
+            y = ["trade_price", "ma5"],
+            kind = "bar"
+        )
+        
+        plt.show()
+        
+        
+        
+     
