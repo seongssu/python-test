1ba55b9cd8bb64230c3a327b7220c9d60917c15d
loveforggong@gmail.com <loveforggong@gmail.com>
2026-05-09 04:56:09 +0900

    feat: 서브플롯을 이용해 그래프를 한 화면에 보이도록 수정했습니다.

--- p02_week_project/graph.py
+++ p02_week_project/graph.py
@@ -2,19 +2,20 @@ import matplotlib.pyplot as plt
 from basic_func import formatting_time
 def graph(result_grouby_ticker):
     
-    for ticker, column in result_grouby_ticker:
+    fig, axes = plt.subplots(3, 1, figsize = (10, 20))
+    
+    for ax, (ticker, column) in zip(axes, result_grouby_ticker):
         
         chart_result = column.plot(
             x = "candle_date_time_kst",
             y = ["trade_price", "ma5"],
             kind = "line",
             title = ticker,
-            
             style = ["-", "--"],
-            
-            alpha = 0.5
+            alpha = 0.5,
+            ax = ax
         )        
         
         formatting_time(chart_result)
-        
-        plt.show()
+    plt.subplots_adjust(hspace= 0.8)    
+    plt.show()
