b21ea9cf1f3fa252845dc412109acfd046156fc8
loveforggong@gmail.com <loveforggong@gmail.com>
2026-05-09 10:31:46 +0900

    feat: db에 저장하고 불러오는 방식으로 변환

--- p02_week_project/main.py
+++ p02_week_project/main.py
@@ -1,7 +1,8 @@
 from p01 import p_one
 from p02 import p_two
 from p03 import p_three
-from basic_func import conversion_datetime, get_sqlite_db
+from basic_func import conversion_datetime, save_sqlite_db
+from Sqlite_Upbit import Sqlite_Upbit
 ###▶▶▶▶▶문제 1번◀◀◀◀◀###
 # candle_date_time_kst : 거래 날짜
 # ticker : 코인명 / price_change : 종가 - 시가
@@ -19,6 +20,10 @@ result_p_two["candle_date_time_kst"] = conversion_datetime(result_p_two["candle_
 print(result_p_two)
 
 ###▶▶▶▶▶문제 3번◀◀◀◀◀###
-sqlite_db = get_sqlite_db(result_p_one, result_p_two)
+save_sqlite_db(result_p_one, result_p_two)
+
+sqlite_upbit = Sqlite_Upbit()
+sqlite_db = sqlite_upbit.load_from_database()
+
 result_p_three = p_three(sqlite_db)
 #print(f"문제3번{result_p_three}")
