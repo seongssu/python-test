8432fcdffce68f48d8d61ea8f54adcfa8d5340ea
loveforggong@gmail.com <loveforggong@gmail.com>
2026-05-09 11:04:03 +0900

    fix: 'sqlite db'에 저장될때 'str'타입으로 저장되기 때문에 matplotlib이 문자열을 제대로 인식못해서 그래프상의 날짜가 이상하게 표시되는 문제 픽스

--- p02_week_project/p03.py
+++ p02_week_project/p03.py
@@ -1,11 +1,14 @@
 from graph import graph
+from basic_func import conversion_datetime
 
 def p_three(result_all_data):
     
-    result_all_data = result_all_data.sort_values(
+    result_all_data["candle_date_time_kst"] = conversion_datetime(result_all_data["candle_date_time_kst"])
+    
+    result_data = result_all_data.sort_values(
         ["ticker", "candle_date_time_kst"]
     )
     
-    result_grouby_ticker = result_all_data.groupby("ticker")
+    result_grouby_ticker = result_data.groupby("ticker")
     
     graph(result_grouby_ticker)
