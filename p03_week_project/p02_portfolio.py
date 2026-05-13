40741d2d7cbd341d261baf761ad12de91c6ff80f
loveforggong@gmail.com <loveforggong@gmail.com>
2026-05-13 14:01:03 +0900

    feat: 성과지표 출력함수 생성

--- p03_week_project/p02_portfolio.py
+++ p03_week_project/p02_portfolio.py
@@ -1,5 +1,6 @@
 from pyupbit_api import PyUpbitApi
 from analyzer_upbit import AnalyzerUpbit
+import datetime
 
 def analysis_money(portfolio, return_rate_ninety):
     current_total_money = 0
@@ -10,25 +11,39 @@ def analysis_money(portfolio, return_rate_ninety):
         weight = data["weight"]
         return_rate = return_rate_ninety[ticker]
         
-        profit = (amount * return_rate) /100
-        profit_money = amount + profit
+        profit_money = (amount * return_rate) /100
+        profit_total_money = amount + profit_money
         
         invest_total_money += amount
-        current_total_money += profit_money
+        current_total_money += profit_total_money
         
         result_portfolio[ticker] = {
             "portfolio_weight" : weight * 100,
             "invest_money" : amount,
             "return_rate" : return_rate,
-            "profit" : profit,
-            "profit_money" : profit_money
+            "profit_money" : profit_money,
+            "profit_total_money" : profit_total_money
         }
     for ticker, data in result_portfolio.items():
-        data["current_weight"] = (data["profit_money"] / current_total_money) * 100   
+        data["current_weight"] = (data["profit_total_money"] / current_total_money) * 100   
     
     total_profit = ((current_total_money/invest_total_money) - 1) * 100
     return result_portfolio, invest_total_money, current_total_money, total_profit
 
+def print_portfolio(result_portfolio, invest_total_money, current_total_money, total_profit):
+    days = 90
+    date_today = datetime.date.today()
+    date_start = date_today - datetime.timedelta(days= days)
+    print("=== 포트폴리오 성과 요약 ===")
+    print(f"투자 기간 {":":>5} {date_start} ~ {date_today} ({days}일)" )
+    print(f"초기 자산 {":":>5} {invest_total_money:,.0f} 원")
+    print(f"현재 자산 {":":>5} {current_total_money:,.0f} 원")
+    print(f"총 수익률 {":":>5} {total_profit:+.2f}%")
+    print(f"MDD {":":>10} \n")
+    print(f"종목별 기여도:")
+    for ticker, data in result_portfolio.items():
+        print(f"{ticker:>10} {"수익률":>5} {data["return_rate"]:>+7.2f}% {"기여":>5} {data["current_weight"]:+7.2f}%")
+    
 portfolio = {
     'KRW-BTC' : {'weight' : 0.4, 'amount' : 4_000_000},
     'KRW-ETH' : {'weight' : 0.3, 'amount' : 3_000_000},
@@ -48,5 +63,6 @@ days_candle_data = pyupbit_api.get_candle_data()
 analyzerupbit = AnalyzerUpbit(current_prices, days_candle_data)
 
 return_rate_ninety = analyzerupbit.get_return_rate_d(invest_day_ago)
-result_portfolio, invest_total_money, current_money, total_profit = analysis_money(portfolio, return_rate_ninety)
+result_portfolio, invest_total_money, current_total_money, total_profit = analysis_money(portfolio, return_rate_ninety)
 
+print_portfolio(result_portfolio, invest_total_money, current_total_money, total_profit)
